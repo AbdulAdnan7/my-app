26e550f914c9589ef31935c4eb494447dd7560b4
Abdul Adnan <peerabduladnan@gmail.com>
2026-03-01 23:53:28 +0530

    Checked how to make pages in Next.Js

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between m-4'>
+
+    <div>
+      <h1>Navaber</h1>
+    </div>
+
+    <div className='flex gap-2'>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/services">Services</Link>
+      <Link href="/contact">Contact</Link>
+    </div>
+
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,7 @@ export default function Home() {
   return (
   <>
   <div className="text-center text-6xl flex justify-center items-center">
-    <h1>this is nextjs</h1>
-    <Image 
-    src='/next.svg'
-    className="bg-white p-4 rounded gap-4 mt-40"
-     alt="next.js logo"
-     width={200}
-     height={40}
-     sizes="100vw"
-     priority
-     />
+    <h1>Tis is mono Text</h1>
   </div>
   </>
   );
